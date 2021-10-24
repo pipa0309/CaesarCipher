374f3d4252df3c5e2ab38d408e6f6e975e21c15e
pipa0309 <melatonin24@yandex.ru>
2021-10-24 08:38:58 +0300

    forCheck

---
+++ CaesarCipher.java
@@ -0,0 +1,38 @@
+package HomeExtraTaskTwo;
+
+public class CaesarCipher {
+
+    static int countShift = 2;
+
+    public static void main(String[] args) {
+
+        String strForEncrypt = enteredText("Hello world!");
+        char[] chForEncrypt = createWords(strForEncrypt);
+        swapCharShift(chForEncrypt);
+        printCharsArray(chForEncrypt);
+    }
+
+    public static char[] createWords(String strForEncrypt) {
+        char[] chForEncrypt = strForEncrypt.toCharArray();
+        return chForEncrypt;
+    }
+
+    private static String enteredText(String strForEncrypt) {
+        return strForEncrypt;
+    }
+
+    private static void printCharsArray(char[] chForEncrypt) {
+        for (int i = 0; i < chForEncrypt.length; i++) {
+            System.out.print(chForEncrypt[i]);
+        }
+    }
+
+    private static void swapCharShift(char[] chForEncrypt) {
+        for (int i = 0; i < chForEncrypt.length - countShift; i++) {
+            char tmp = chForEncrypt[i];
+            chForEncrypt[i] = chForEncrypt[i + countShift];
+            chForEncrypt[i + countShift] = tmp;
+        }
+    }
+}
+
